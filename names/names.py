be51fee4ddef2eacb6ae727023493efbadd8c15e
David Horstman <davidzilla27@gmail.com>
2020-07-17 11:36:04 -0400

    Runtime optimization

--- names/names.py
+++ names/names.py
@@ -1,26 +1,25 @@
 import time
-
+from binary_search_tree import BSTNode
 start_time = time.time()
-
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+with open('names_1.txt', 'r') as f:
+    names_1 = f.read().split("\n")
+    names_1_tree = BSTNode(names_1.pop(0))
+    for name in names_1:
+        names_1_tree.insert(name)
+
+with open('names_2.txt', 'r') as f:
+    names_2 = f.read().split("\n")
+    for name in names_2:
+        if names_1_tree.contains(name):
+            duplicates.append(name)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
