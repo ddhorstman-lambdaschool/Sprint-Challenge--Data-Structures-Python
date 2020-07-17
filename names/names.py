872c0bf6d9a11d6f9df2f1d5548298f201a3c795
David Horstman <davidzilla27@gmail.com>
2020-07-17 13:25:12 -0400

    Clean up

--- names/names.py
+++ names/names.py
@@ -3,35 +3,40 @@ from binary_search_tree import BSTNode
 start_time = time.time()
 duplicates = []  # Return the list of duplicates in this data structure
 
+
 # Starter code has a complexity of O(n) * O(n).
 # If the lists have the same length, that becomes O(n^2)
 
-# Original solution: BST (74 ms)
+
+# # Original solution: BST (74 ms)
 # with open('names_1.txt', 'r') as f:
 #     names_1 = f.read().split("\n")
 #     names_1_tree = BSTNode(names_1.pop(0))
 #     for name in names_1:
 #         names_1_tree.insert(name)
-
+#
 # with open('names_2.txt', 'r') as f:
 #     names_2 = f.read().split("\n")
 #     for name in names_2:
 #         if names_1_tree.contains(name):
 #             duplicates.append(name)
 
-# Fastest solution: sets (3 ms)
+
+# # Fastest solution: sets (3 ms)
 # with open('names_1.txt', 'r') as f:
 #     names_1 = set(f.read().split("\n"))
 # with open('names_2.txt', 'r') as f:
 #     names_2 = set(f.read().split("\n"))
 # duplicates = names_1 & names_2
 
-# List-only solution using built-in methods: (680 ms)
+
+# # List-only solution using built-in methods: (680 ms)
 # with open('names_1.txt', 'r') as f:
 #     names_1 = f.read().split("\n")
 # with open('names_2.txt', 'r') as f:
 #     names_2 = f.read().split("\n")
 #     duplicates = [n for n in names_2 if n in names_1]
+
 
 # Custom list-only solution: (340 ms)
 def midpoint_search(needle, haystack):
@@ -53,8 +58,7 @@ with open('names_2.txt', 'r') as f:
     duplicates = [n for n in names_2 if midpoint_search(n, names_1)]
 
 
-
-
+# Shared code
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
